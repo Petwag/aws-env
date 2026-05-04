d8facd3222563cd3ad63cf09bda4e416bf86f3e7
Petwag <fhortet.petwag@gmail.com>
2026-05-04 15:30:04 +0100

    feat(): list local aws profiles instead of asking for input. Use aws cli instead of sdk

--- src/inputs/profile.rs
+++ src/inputs/profile.rs
@@ -1,4 +1,5 @@
-use cliclack::{input, note};
+use cliclack::{note, select};
+use std::process::Command;
 
 pub fn get_profile(profile: Option<String>) -> Result<String, Box<dyn std::error::Error>> {
     let profile = match profile {
@@ -6,8 +7,30 @@ pub fn get_profile(profile: Option<String>) -> Result<String, Box<dyn std::error
             note("Using profile", &p)?;
             p
         }
-        None => input("Select AWS profile").interact()?,
+        None => {
+            let profiles = get_aws_profiles()?;
+
+            let items: Vec<(String, String, String)> = profiles
+                .iter()
+                .map(|p| (p.clone(), p.clone(), "".to_string()))
+                .collect();
+
+            select("Select the AWS profile")
+                .items(&items)
+                .interact()?
+        }
     };
 
     Ok(profile)
+}
+
+
+fn get_aws_profiles() -> Result<Vec<String>, Box<dyn std::error::Error>> {
+    let output = Command::new("aws")
+        .args(["configure", "list-profiles"])
+        .output()?;
+
+    let stdout = String::from_utf8(output.stdout)?;
+
+    Ok(stdout.lines().map(|s| s.to_string()).collect())
 }
